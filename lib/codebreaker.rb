e2850e3b8eb10fd019b65f248bbe76aa19c4c2ab
eoris <eoris.work@gmail.com>
2015-11-06 14:56:18 +0200

    first commit

---
+++ lib/codebreaker.rb
@@ -0,0 +1,17 @@
+require "codebreaker/version"
+
+module Codebreaker
+  class Game
+    def initialize
+      @secret_code = ''
+    end
+
+    def start
+      @secret_code = secret_code
+    end
+
+    def secret_code
+      (1..6).to_a.sample(4)
+    end
+  end
+end
